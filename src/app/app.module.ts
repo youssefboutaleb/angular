8d0f2d2fc0c5d1df7f6feebbd1f7b3241bd7b830
youssef boutaleb <phoneyoussef178@gmail.com>
2023-11-07 11:00:17 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MemberComponent } from './member/member.component';
+import { DoctorComponent } from './doctor/doctor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MemberComponent,
+    DoctorComponent
   ],
   imports: [
     BrowserModule,
