1032a4f660efc56a0b54eb32d7e00d920e340399
youssef boutaleb <phoneyoussef178@gmail.com>
2023-11-07 11:15:36 +0100

    initial commit

--- src/app/member/member.component.ts
+++ src/app/member/member.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./member.component.css']
 })
 export class MemberComponent {
-
+dataSource:any[]=[
+  {id:"1234587",
+  cin:"13454645",
+name:"youssef",
+createdDate:"12/12/12",
+cv:"lien",
+type:"teacher"},
+  {id:"5555555",
+  cin:"13454645",
+name:"youssef",
+createdDate:"12/12/12",
+cv:"lien",
+type:"teacher"},
+  {id:"156465",
+  cin:"13454645",
+name:"youssef",
+createdDate:"12/12/12",
+cv:"lien",
+type:"teacher"},
+]
 }
